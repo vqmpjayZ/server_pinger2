1c92efc38f04703cbf8427fb7d66f986910fed1f
vqmpjay <bakirbutkovic13@gmail.com>
2025-07-30 20:16:26 +0200

    Create Node.js

---
+++ Node.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const axios = require("axios");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const renderURL = "-------";
+
+function pingRender() {
+  setInterval(async () => {
+    try {
+      const res = await axios.get(renderURL);
+      console.log(`[+] Pinged Render | Status: ${res.status}`);
+    } catch (err) {
+      console.log(`[-] Failed to ping Render | ${err.message}`);
+    }
+  }, 45000);
+}
+
+app.get("/", (req, res) => {
+  res.send("✅ Replit Ping Server is alive");
+});
+
+app.listen(PORT, () => {
+  console.log(`✅ Replit server running`);
+  pingRender();
+});
